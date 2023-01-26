a592c3cfbdd689af6ac6386dd9e509c05287ef5d
Yedigaryan <davit.yedigaryan.1993@gmail.com>
2023-01-26 17:19:49 +0400

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || '3000';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(JSON.stringify({success: true}));
 });
 server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
