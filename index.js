dc53eec6ec36ee28c4b3eec0db575b5fadc289b3
nlbo <eric.nalbandyan@teracloud.com>
2023-01-31 17:43:14 +0400

    change

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+let num = 0;
+
+
+app.get('/:id', (req, res) => {
+  if(req.params?.id) {
+    num = Number(req.params.id);
+  }
+  res.json({success: num})
+})
 
 app.get('/', (req, res) => {
-  res.json({success: 0})
+  res.json({success: num})
 })
 
 app.listen(port, () => {
